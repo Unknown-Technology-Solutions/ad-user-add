3e337ba771e6388081665d066affb42d130a2ea0
Zane D. R <zane.reick@unknownts.com>
2023-09-01 15:08:59 -0500

    Automatically attempt STARTTLS

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import json
 configuration = json.load(open("config.json","r"))
 
 server = Server(configuration['SERVER'], get_info=ALL)
-conn = Connection(server, user=configuration['DOMAIN']+"\\"+configuration['USERNAME'], password=configuration['PASSWORD'], authentication=NTLM)
-
-conn.extend.standard.who_am_i()
+conn = Connection(server, user=configuration['DOMAIN']+"\\"+configuration['USERNAME'], password=configuration['PASSWORD'], authentication=NTLM, auto_bind=True)
+if conn.start_tls == True:
+    print("TLS STARTED")
+#    print(conn)
+else:
+    print("TLS NOT STARTED")
+#    print(conn)
+print(conn.extend.standard.who_am_i())
